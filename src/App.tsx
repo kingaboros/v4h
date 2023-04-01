c65a5e682eeca0951ff1c110a3bd13a46849b916
kinga <kinga.boros20@gmail.com>
2023-04-01 18:43:01 +0300

    hero & about section done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Navigation from './components/navigation/Navigation';
+import Homepage from './screens/Homepage';
 
 function App() {
   return (
     <div className="app">
       <Navigation />
+      <Homepage />
     </div>
   );
 }
